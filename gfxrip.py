7b23ba6bf27493babbf5cf32d035b912aac7711c
furrykef <furrykef@localhost>
2009-06-06 03:24:58 +0000

    gfxrip.py rips sprites now

--- gfxrip.py
+++ gfxrip.py
@@ -5,6 +5,10 @@ import sys
 from PIL import Image
 
 
+# Note: needs trailing slash
+ROMDIR = 'ROMs/dkong/'
+
+
 tile_palette = [(0, 0, 0),
                 (0x88, 0x88, 0x88),
                 (0xcc, 0xcc, 0xcc),
@@ -15,10 +19,13 @@ tile_files = [
     "v_3pt.bin"
 ]
 
-spr_files = [
+top_spr_files = [
     "l_4m_b.bin",
-    "l_4n_b.bin",
     "l_4r_b.bin",
+]
+
+bottom_spr_files = [
+    "l_4n_b.bin",
     "l_4s_b.bin"
 ]
 
@@ -27,17 +34,8 @@ def main(argv=None):
     if argv is None:
         argv = sys.argv[1:]
 
-    tiledata = [0] * 0x4000
-    bitplane = 0
-    for filename in tile_files:
-        with open('ROMs/dkong/' + filename, 'rb') as f:
-            data = f.read()
-        for offset, value in enumerate(data):
-            value = ord(value)
-            for bit in range(7, -1, -1):
-                tiledata[offset*8 + bit] |= (value & 1) << bitplane
-                value >>= 1
-        bitplane += 1
+    ### TILES ###
+    tiledata = decode(tile_files)
 
     # Convert paletted values to RGB values
     tiledata = [tile_palette[x] for x in tiledata]
@@ -53,12 +51,49 @@ def main(argv=None):
 
     tile_img.save("tiles.bmp")
 
-    if 0:
-        sprdata = []
-        for filename in tile_files:
-            with open(filename, 'rb') as f:
-                pass
-    
+
+    ### SPRITES ###
+    top_spr_data = decode(top_spr_files)
+    bottom_spr_data = decode(bottom_spr_files)
+
+    # Convert paletted values to RGB values
+    top_spr_data = [tile_palette[x] for x in top_spr_data]
+    bottom_spr_data = [tile_palette[x] for x in bottom_spr_data]
+
+    spr_img = Image.new("RGB", (2048, 16))
+    for offset in range(0, len(top_spr_data), 64):
+        tile_num = offset/64
+
+        # Hack to reverse horizontal order of tiles within sprites
+        if tile_num % 2 == 0:
+            tile_num += 1
+        else:
+            tile_num -= 1
+
+        for y in range(8):
+            for x in range(8):
+                # Note that we're rotating each tile 90 degrees right as we do this
+                # Hence putting pixels at (7-y, x) instead of (x, y)
+                spr_img.putpixel((tile_num*8+(7-y), x), top_spr_data[offset+y*8+x])
+                spr_img.putpixel((tile_num*8+(7-y), x+8), bottom_spr_data[offset+y*8+x])
+
+    spr_img.save("sprites.bmp")
+
+
+def decode(file_list):
+    outdata = [0] * 0x4000
+    bitplane = 0
+    for filename in file_list:
+        with open(ROMDIR + filename, 'rb') as f:
+            indata = f.read()
+        for offset, value in enumerate(indata):
+            value = ord(value)
+            for bit in range(7, -1, -1):
+                outdata[offset*8 + bit] |= (value & 1) << bitplane
+                value >>= 1
+        bitplane += 1
+    return outdata
+
 
 if __name__ == '__main__':
     main()
